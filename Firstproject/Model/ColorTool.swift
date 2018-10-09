98bcdde09af1bb8c7762208b21f273cdccb6d3eb
Andrew <andrewgardner@live.com>
2018-10-09 12:20:39 -0600

    added model group and renamed files

---
+++ Firstproject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  Firstproject
+//
+//  Created by Gardner, Andrew on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
